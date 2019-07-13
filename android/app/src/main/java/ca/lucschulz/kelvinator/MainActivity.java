1bf4900462698c77a7a7a4d1d8778d0c458813ed
Luc Schulz <luc@lucschulz.com>
2019-07-13 13:50:15 -0400

    UnitConverter

--- android/app/src/main/java/ca/lucschulz/kelvinator/MainActivity.java
+++ android/app/src/main/java/ca/lucschulz/kelvinator/MainActivity.java
@@ -1,18 +1,20 @@
 package ca.lucschulz.kelvinator;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,15 +29,7 @@ public class MainActivity extends AppCompatActivity {
         RadioButton btnCelsius = findViewById(R.id.radCelsius);
         btnCelsius.setChecked(true);
 
-        EditText inputValue = findViewById(R.id.txtInputTemp);
-        inputValue.setText("0", TextView.BufferType.EDITABLE);
-
-        inputValue.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                return false;
-            }
-        });
+        configureInputValueHandling();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -67,5 +61,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void configureInputValueHandling() {
+        EditText inputValue = findViewById(R.id.txtInputTemp);
+        inputValue.setText("0", TextView.BufferType.EDITABLE);
+
+        final Activity act = this;
+
+        inputValue.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                UnitConverter uc = new UnitConverter(UnitConverter.Units.K, -273);
+
+                Toast.makeText(act, s.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- android/app/src/main/java/ca/lucschulz/kelvinator/UnitConverter.java
+++ android/app/src/main/java/ca/lucschulz/kelvinator/UnitConverter.java
@@ -1,14 +1,82 @@
 package ca.lucschulz.kelvinator;
 
-
-public class UnitConverter {
-
 enum Units {
     C,
     F,
     K
 }
-    public UnitConverter(Units inputUnit, int inputValue) {
 
+class UnitConverter {
+
+    private Units inputUnit;
+    private int inputValue;
+
+    private double outputC;
+    private double outputF;
+    private double outputK;
+
+    public double getOutputC() {
+        return outputC;
+    }
+
+    public double getOutputF() {
+        return outputF;
+    }
+
+    public double getOutputK() {
+        return outputK;
+    }
+
+    UnitConverter(Units inputUnit, int inputValue) {
+        this.inputUnit = inputUnit;
+        this.inputValue = inputValue;
+
+        convertTemps();
+    }
+
+    private void convertTemps() {
+        switch (inputUnit)
+        {
+            case C:
+                outputK = convertCtoK(inputValue);
+                outputF = convertCtoF(inputValue);
+                break;
+
+            case F:
+                outputK = convertFtoK(inputValue);
+                outputC = convertFtoC(inputValue);
+                break;
+
+            case K:
+                outputF = convertKtoF(inputValue);
+                outputC = convertKtoC(inputValue);
+                break;
+        }
+    }
+
+    private double convertCtoF(double c) {
+        return(1.8 * c) + 32;
+    }
+
+    private double convertFtoC(double f) {
+        return (f - 32) / 1.8;
+    }
+
+    private double convertCtoK(double c) {
+        return c + 273.15;
+    }
+
+    private double convertKtoC(double k) {
+        return k - 273.15;
+    }
+
+    private double convertKtoF(double k) {
+        double c = convertKtoC(k);
+        return convertCtoF(c);
+    }
+
+    private double convertFtoK(double f) {
+        double c = convertFtoC(f);
+        return convertCtoK(c);
     }
 }
