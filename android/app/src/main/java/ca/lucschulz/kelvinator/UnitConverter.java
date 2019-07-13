88be6deaada33755ffa2369de509e006f9cd53ac
Luc Schulz <luc@lucschulz.com>
2019-07-12 22:24:38 -0400

    Layouting

---
+++ android/app/src/main/java/ca/lucschulz/kelvinator/UnitConverter.java
@@ -0,0 +1,14 @@
+package ca.lucschulz.kelvinator;
+
+
+public class UnitConverter {
+
+enum Units {
+    C,
+    F,
+    K
+}
+    public UnitConverter(Units inputUnit, int inputValue) {
+
+    }
+}
