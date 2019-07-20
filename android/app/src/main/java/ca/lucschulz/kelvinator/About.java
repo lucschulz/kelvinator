2804cc7ad617d80f8453c7f28ef14840e8d8bb4c
Luc Schulz <luc@lucschulz.com>
2019-07-20 18:47:51 -0400

    email button

--- android/app/src/main/java/ca/lucschulz/kelvinator/About.java
+++ android/app/src/main/java/ca/lucschulz/kelvinator/About.java
@@ -1,8 +1,8 @@
 package ca.lucschulz.kelvinator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -20,11 +20,24 @@ public class About extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                createEmailMessage();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void createEmailMessage() {
+        Intent email = new Intent(Intent.ACTION_SEND);
+        email.putExtra(Intent.EXTRA_EMAIL, new String[]{ "info@tesscorp.ca"});
+        email.putExtra(Intent.EXTRA_SUBJECT, "Kelvinator");
+        email.putExtra(Intent.EXTRA_TEXT, "Enter message here.");
+
+        //need this to prompts email client only
+        email.setType("message/rfc822");
+
+        startActivity(Intent.createChooser(email, "Choose an Email client :"));
     }
 
 }
